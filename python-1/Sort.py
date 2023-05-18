343594a7bd39350c63220aa4bfb3d185e245c099
vickneee <victoria.vavulina@outlook.com>
2023-05-19 01:01:00 +0300

    Initial commit

--- python-1/Sort.py
+++ python-1/Sort.py
@@ -3,10 +3,10 @@ b = float(input("Enter 2st number: "))
 c = float(input("Enter 3st number: "))
 sorting = input("Choose sorting order: N/L ")
 numbers = [a, b, c]
-if ((sorting == "N") or (sorting == "n")):
+if (sorting == "N") or (sorting == "n"):
 	numbers.sort()
 	print(numbers)
-elif ((sorting == "L") or (sorting == "l")):
+elif (sorting == "L") or (sorting == "l"):
 	numbers.sort(reverse=True)
 	print(numbers)
 else:
