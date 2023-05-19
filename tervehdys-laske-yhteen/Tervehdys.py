9de80c236900d0bb906f2b823c3278a43a96cc8d
vickneee <victoria.vavulina@outlook.com>
2023-05-19 14:27:25 +0300

    Line spacing corr

--- tervehdys-laske-yhteen/Tervehdys.py
+++ tervehdys-laske-yhteen/Tervehdys.py
@@ -4,7 +4,6 @@ Name = input("Name: ")
 Luku1 = int(input("Anna luku 1: "))
 Luku2 = int(input("Anna luku 2: "))
 
-
 Tervehdys = mF.Greeting(Name)
 Summa = mF.LaskeYhteen(Luku1, Luku2)
 print(Tervehdys, Summa)
