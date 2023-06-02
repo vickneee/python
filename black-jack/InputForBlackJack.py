f8ba219c74aaead7c045033df33841abbd2f3fa2
vickneee <victoria.vavulina@outlook.com>
2023-06-02 16:45:33 +0300

    Style corrections

--- black-jack/InputForBlackJack.py
+++ black-jack/InputForBlackJack.py
@@ -1,7 +1,7 @@
-# \u2660 = ♠ Spade
-# \u2663 = ♣ Club
-# \u2665 = ♥ Heart
-# \u2666 = ♦ Diamond
+#  ♠ Spade
+#  ♣ Club
+#  ♥ Heart
+#  ♦ Diamond
 
 Rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 Suit = ['♠', '♣', '♥', '♦']
